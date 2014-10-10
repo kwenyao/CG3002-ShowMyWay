15b70a4203506bf04c30d2719569d9b3b33288b6
John Kuan <johnkuan108@gmail.com>
2014-10-10 22:49:45 +0800

    added simulated testing of COM1 level 2 with actual data

--- wifi.py
+++ wifi.py
@@ -1,4 +1,4 @@
-from trilateration import TrilaterationCalculation
+from trilateration2 import TrilaterationCalculation
 from accessPoints import AccessPoints
 
 class Wifi():
@@ -7,12 +7,18 @@ class Wifi():
 		self.trilateration = TrilaterationCalculation()
 
 		#For testing#
-		# ap_list = [{'address':"29:11:A1:8B:C2", 'essid':"accessPoints-101", 'frequency':"2.41", 'signal': -87, 'distance': self.ap.calculateDistanceFromAP(-87,2.41)},
-		# 			 {'address':"9A:22:5B:1C:D4", 'essid':"accessPoints-102", 'frequency':"2.41", 'signal': -95, 'distance': self.ap.calculateDistanceFromAP(-95,2.41)}, 
-		# 			 {'address':"F9:33:0A:92:9C", 'essid':"accessPoints-103", 'frequency':"2.41", 'signal': -90, 'distance': self.ap.calculateDistanceFromAP(-90,2.41)}, 
-		# 			 {'address':"B1:44:A6:BB:EC", 'essid':"accessPoints-104", 'frequency':"2.41", 'signal': -90, 'distance': self.ap.calculateDistanceFromAP(-90,2.41)}]
+		# ap_list = [{'address':"29:11:A1:8B:C2", 'essid':"accessPoints-101", 'frequency':"2.41", 'signal': -45, 'distance': self.ap.calculateDistanceFromAP(-45,2.41)},
+		# 			 {'address':"9A:22:5B:1C:D4", 'essid':"accessPoints-102", 'frequency':"2.41", 'signal': -56, 'distance': self.ap.calculateDistanceFromAP(-56,2.41)}, 
+		# 			 {'address':"F9:33:0A:92:9C", 'essid':"accessPoints-103", 'frequency':"2.41", 'signal': -54, 'distance': self.ap.calculateDistanceFromAP(-54,2.41)}, 
+		# 			 {'address':"B1:44:A6:BB:EC", 'essid':"accessPoints-104", 'frequency':"2.41", 'signal': -68, 'distance': self.ap.calculateDistanceFromAP(-68,2.41)}]
 
-		ap_list = self.ap.getAccessPoints()
+		ap_list = [{'address':"28:93:FE:D3:8B", 'essid':"arc-0241-a", 'frequency':"2.412", 'signal': -35, 'distance': self.ap.calculateDistanceFromAP(-35,2.412)},
+					 {'address':"E8:BA:70:52:3B", 'essid':"arc-0205-a", 'frequency':"2.437", 'signal': -61, 'distance': self.ap.calculateDistanceFromAP(-61,2.437)}, 
+					 {'address':"E8:BA:70:52:BF", 'essid':"arc-0205-b", 'frequency':"2.462", 'signal': -57, 'distance': self.ap.calculateDistanceFromAP(-57,2.462)}, 
+					 {'address':"E8:BA:70:52:1E", 'essid':"arc-0231", 'frequency':"2.41", 'signal': -80, 'distance': self.ap.calculateDistanceFromAP(-80,2.41)}]
+
+
+		# ap_list = self.ap.getAccessPoints()
 		ap_list = self.ap.sortAccessPoints(ap_list)
 		print "matches: " + str(len(ap_list))
 
