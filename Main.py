da1e9392667111919e4406f39497fd30342d4b4c
weijian19391 <weijian19391@gmail.com>
2014-10-13 09:17:52 +0800

    Included path navigation file

---
+++ Main.py
@@ -0,0 +1,145 @@
+import getMap
+import visualiseMap
+import getPath
+import math
+#----------------------------------------------------------------------------------------------------------------------------------------
+#GLOBAL VARIABLES
+#----------------------------------------------------------------------------------------------------------------------------------------
+current_coor = (400, 125)	#current coordinate of the user
+map_north = -1				#bearing of the north given by the map
+direction_faced = 180		#direction that user is facing as determined by the Mega compass, a bearing from north, rotate anti-clockwise
+step_direction = []			#queue of directions given by the Mega Accelerometer 
+ultrasound_data = []		#list of ultrasound data, each element is a tuple. e.g. sensor_number:distance
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+#CONSTANTS
+#----------------------------------------------------------------------------------------------------------------------------------------
+step_length = 0.8 				#length of each step of user, measured in meters
+radius_of_closeness = 0.3 		#this radius determines the level of closeness we need to get to the node to determine that use is actually at that node
+orientation_degree_error= 5 	#the degree of deviant the user can be wrt to the actual bearing he shld be walking straight to reach the node.
+freq_instructions = 5			#the time (in minutes) between consecutive "walk straight" instructions
+#----------------------------------------------------------------------------------------------------------------------------------------
+#FLAGS
+#----------------------------------------------------------------------------------------------------------------------------------------
+object_detection = 0		#set to 1 when object detected
+user_input = 0 				#set to 1 when user presses the keyboard
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+#INITIALISATION PHASE
+#----------------------------------------------------------------------------------------------------------------------------------------
+
+#download all maps
+Map = getMap.getMap()
+Map.downloadAllMaps()
+
+#set the current map user is in
+packet = Map.getFromCache("toximble5")
+Map_nodes = packet['map']
+
+#initialise visualisation tool
+visual = visualiseMap.visualiseMap(1300,1300)
+visual.setMap(Map_nodes,0)
+
+#initialise calculate_path object with the current number of vertex
+calculate_path = getPath.getPath(Map_nodes)
+
+#initialise the starting and ending vertex
+start_point = '3'
+end_point = '9'
+
+#determine the shortest path to take
+calculate_path.formAdjlist(Map_nodes)
+calculate_path.shortestPath(start_point)
+route = calculate_path.routeToTravel(start_point, end_point)
+
+#get north
+map_north = int(packet['info'].get("northAt"))
+
+#include path to be taken in visualisation tool
+route_nodes = visual.getRouteNodes(Map_nodes, route)
+visual.setMap(route_nodes,1)
+
+#see the map
+#visual.printMap()
+
+#initialise starting coordinate and ending coordinate, next node to travel to.
+current_node = route_nodes.get(start_point) #gets reassign to the next node once i reach the next node
+current_coor_read = (int(current_node.get('x')), int(current_node.get('y'))) #current coordinate of user that is read from the global variables
+final_node = route_nodes.get(end_point)
+final_coor = (int(final_node.get('x')), int(final_node.get('y')))
+next_node_to_travel = route_nodes.get((current_node.get('linkTo'))[0])
+next_coor = (int(next_node_to_travel.get('x')), int(next_node_to_travel.get('y')))
+
+#declare variables
+offset = -1 			#the degrees to offset based on the movement to be make 
+bearing_to_face = -1 	#the bearing user should face to walk straight to reach the next point
+time_since_last = 1200 	#the timing that the last instruction for walk straight is issued, initialised to the time pi is started up
+current_time = 1200		#current time 	
+dist_to_next_node = -1 	#the distance to the next node
+num_steps_to_next = -1 	#number of steps to the next node
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+#GUIDING PHASE
+#----------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------Directing----------------------------------------------------------------------
+#assume user already in the correct orientation
+dist_to_next_node = math.sqrt((next_coor[0] - current_coor_read[0])**2 + (next_coor[1] - current_coor_read[1])**2)
+num_steps_to_next = dist_to_next_node/step_length
+print "walk forward " , num_steps_to_next , "steps"
+time_since_last = 1200 # get the current time here.
+
+#condition to exit navigation is when user reaches within 30cm of the end coordinate 
+while abs(current_coor_read[0]-final_coor[0]) >radius_of_closeness*100 and abs(current_coor_read[1]-final_coor[1]) > radius_of_closeness*100 :
+	current_coor_read = current_coor
+	#check if i have reach the next node
+	if (abs(current_coor_read[0]-next_coor[0]) <radius_of_closeness*100 and abs(current_coor_read[1]-next_coor[1]) < radius_of_closeness*100) == True:
+		current_node = next_node_to_travel
+		next_node_to_travel = route_nodes.get((current_node.get('linkTo'))[0])
+		next_coor = (int(next_node_to_travel.get('x')), int(next_node_to_travel.get('y')))
+		print "you have reached ", current_node["name"]
+		#call wait function for the talking to finish
+
+	#This block of code checks the bearing user shld face to move forward
+	if current_coor_read[0] < next_coor[0] : #x-coor increase
+		if current_coor_read[1] < next_coor[1] : # y-coor increase
+			offset = math.degrees(math.atan2((next_coor[0] - current_coor_read[0]) ,  (next_coor[1]-current_coor_read[1])))
+		elif current_coor_read[1] == next_coor[1] : # y-coor remains constant
+			offset = 90
+		elif current_coor_read[1] > next_coor[1] : # y-coor decrease
+			offset =180 -  math.degrees(math.atan2((next_coor[0] - current_coor_read[0]) ,  (current_coor_read[1]-next_coor[1])))
+	elif current_coor_read[0] > next_coor[0] : #x-coor decrease
+		if current_coor_read[1] < next_coor[1] : # y-coor increase
+			offset =360 - math.degrees(math.atan2((current_coor_read[0]-next_coor[0]) ,  (next_coor[1]-current_coor_read[1])))
+		elif current_coor_read[1] == next_coor[1] : # y-coor remains constant
+			offset = 270
+		elif current_coor_read[1] > next_coor[1] : # y-coor decrease
+			offset =180 + math.degrees(math.atan2((current_coor_read[0]-next_coor[0]) ,  (current_coor_read[1]-next_coor[1])))
+	else: #x-coor remains constant
+		if current_coor_read[1] < next_coor[1] :
+			offset = 360
+		else :
+			offset = 180
+	bearing_to_face = (map_north + offset) % 360		
+	print bearing_to_face
+	if abs(bearing_to_face - direction_faced) > orientation_degree_error:
+		if bearing_to_face < direction_faced:
+			print "turn left", direction_faced - bearing_to_face, "degrees"
+		else:
+			print "turn right" , bearing_to_face - direction_faced, " degrees"
+		#call wait function here, to give user time to rotate	
+	else:
+		#guide user to walk straight
+		if (current_time - time_since_last) >= freq_instructions:
+			time_since_last = current_time
+			dist_to_next_node = math.sqrt((next_coor[0] - current_coor_read[0])**2 + (next_coor[1] - current_coor_read[1])**2)
+			num_steps_to_next = dist_to_next_node/step_length
+			print "walk forward" , num_steps_to_next , "steps"
+
+	break		
+#---------------------------------------------------------Wifi-trilateration-------------------------------------------------------------
+
+		
+#---------------------------------------------------------Receiving Data From Arduino----------------------------------------------------
+
+#---------------------------------------------------------Obstacle handling--------------------------------------------------------------
---
+++ getPath.py
@@ -0,0 +1,68 @@
+from heapq import *
+import math
+
+class getPath :
+	def __init__(self, Map_nodes):
+		self.graph_adjlist = []
+		self.parent = {}
+		self.dist = {}
+		self.PQ = []
+		for i in Map_nodes:
+			self.graph_adjlist.append([])
+			self.parent[i] = -1
+			self.dist[i] = 1000000000000
+
+	def formAdjlist(self, Map_nodes):
+		for vertex, details in Map_nodes.iteritems():
+		   # print vertex, details['x'], details['y'], details['linkTo']
+		    neighbour_list = details['linkTo']
+		    for neighbour in neighbour_list:
+		    	weight = math.sqrt(((int(Map_nodes[neighbour]['y']) - int(details['y']))**2) + (int(Map_nodes[neighbour]['x']) - int(details['x']))**2)
+		    	self.graph_adjlist[int(vertex)-1].append([weight, neighbour])
+
+	def printAdjlist(self):
+		for i in self.graph_adjlist:
+			print i
+
+	def shortestPath(self, start_point):
+		self.dist[start_point] = 0
+		first_node = (0,start_point)
+		heappush(self.PQ, first_node)
+
+		while (len(self.PQ) != 0):
+			front = self.PQ[0];
+			del self.PQ[0]
+			# print front
+			frontVertex = front[1];
+			if (front[0] == self.dist[frontVertex]): 
+				for neighbour in self.graph_adjlist[int(frontVertex)-1]:
+					neighbourVertex = neighbour[1]
+					neighbourWeight = neighbour[0]
+					# print neighbourVertex
+					# print self.dist[neighbourVertex],
+					# print self.dist[frontVertex] ,
+					# print neighbourWeight
+					if (self.dist[neighbourVertex] > self.dist[frontVertex] + neighbourWeight):
+						self.dist[neighbourVertex] = self.dist[frontVertex] + neighbourWeight
+						self.parent[neighbourVertex] =  frontVertex
+						new_entry = (self.dist[neighbourVertex], neighbourVertex)
+						heappush(self.PQ, new_entry)
+	
+	def printParent(self):
+		print self.parent
+
+	def routeToTravel(self,start_point, end_point):
+		route = [end_point,]
+		while (self.parent[end_point] != start_point):
+			end_point = self.parent[end_point]
+			route.append(end_point)
+		route.append(start_point)
+		route.reverse()
+		return route
+		          
+
+def mult(x,y):
+	return x * y
+
+print map(mult,[2,3],[4,6])
+
